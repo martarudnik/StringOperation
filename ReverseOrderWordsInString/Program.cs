e2f3f8c7b402916d7e6a5732f8fc0f07e0d8234e
Marta Rudnik <marta.rudnik@intive.com>
2020-09-07 01:13:56 +0200

    String operation - next tasks

--- ReverseOrderWordsInString/Program.cs
+++ ReverseOrderWordsInString/Program.cs
@@ -7,9 +7,26 @@ namespace ReverseOrderWordsInString
         static void Main(string[] args)
         {
             string inputString = "Welcome in my project";
+            string inpitPolodrome = "madam";
+            string inputString1 = "abcd";
+            string inputString2 = "hello world";
+
             StringOperationClass stringOperation = new StringOperationClass();
+
             string outputReverseOrderWordsInString = stringOperation.ReverseOrderWordsInString(inputString);
+            bool outputPolidrome = stringOperation.CheckIfPalindrome(inpitPolodrome);
+            string outputReverseString = stringOperation.ReverseString(inputString);
+            string possibleSubstringInString = stringOperation.PossibleSubstringInString(inputString1);
+            string removeDuplicateCharactersInString = stringOperation.RemoveDuplicateCharactersInString(inputString);
+            string countTheOccurrenceEachCharacterInString = stringOperation.CountTheOccurrenceEachCharacterInString(inputString2);
+
             Console.WriteLine(outputReverseOrderWordsInString);
+            Console.WriteLine(outputPolidrome);
+            Console.WriteLine(outputReverseString);
+            Console.WriteLine(possibleSubstringInString);
+            Console.WriteLine(removeDuplicateCharactersInString);
+            Console.WriteLine(countTheOccurrenceEachCharacterInString);
+
             Console.ReadKey();
         }
     }
--- ReverseOrderWordsInString/StringOperationClass.cs
+++ ReverseOrderWordsInString/StringOperationClass.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Text;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
 
 namespace ReverseOrderWordsInString
 {
-   public class StringOperationClass
+    public class StringOperationClass
     {
         public string ReverseOrderWordsInString(string input)
         {
@@ -21,6 +20,73 @@ namespace ReverseOrderWordsInString
                 }
             }
             return output.ToString();
+        }
+        public bool CheckIfPalindrome(string input)
+        {
+            for (int i = 0, j = input.Length - 1; i < input.Length / 2; i++, j--)
+            {
+                if (input[i] == input[j])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public string ReverseString(string input)
+        {
+            var inputArray = input.ToCharArray();
+            StringBuilder output = new StringBuilder();
+            for (int i = inputArray.Length - 1; i < inputArray.Length; i--)
+            {
+                if (i >= 0)
+                    output.Append(inputArray[i]);
+            }
+            return output.ToString();
+        }
+        public string PossibleSubstringInString(string input)
+        {
+            StringBuilder output = new StringBuilder();
+            for (int i = 0; i < input.Length; ++i)
+            {
+                StringBuilder subString = new StringBuilder(input.Length - i);
+                for (int j = i; j < input.Length; ++j)
+                {
+                    subString.Append(input[j]);
+                    output.Append(subString + " ");
+                }
+            }
+            return output.ToString();
+        }
+        public string RemoveDuplicateCharactersInString(string input)
+        {
+            return new string(input.ToCharArray().Distinct().ToArray());
+        }
+        public string CountTheOccurrenceEachCharacterInString(string input)
+        {
+            StringBuilder output = new StringBuilder();
+            Dictionary<char, int> characterCount = new Dictionary<char, int>();
+
+            foreach (var character in input)
+            {
+                if (character != ' ')
+                {
+                    if (!characterCount.ContainsKey(character))
+                    {
+                        characterCount.Add(character, 1);
+                    }
+                    else
+                    {
+                        characterCount[character]++;
+                    }
+                }
+
+            }
+            foreach (var character in characterCount)
+            {
+                output.AppendFormat("{0} - {1} {2}", character.Key, character.Value, Environment.NewLine);
+            }
+
+          return output.ToString();
         }
     }
 }
